5bd25355d07b8463dbe1938194ce1430093af94e
JunHatanaka <nextseason6@gmail.com>
2021-10-22 00:43:33 +0900

    Added sign-in function.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
 import SignUp from './components/userAuth/SignUp'
+import SignIn from './components/userAuth/SignIn'
 
 function App() {
   return (
     <div className="app">
-      <SignUp />
+      <SignIn />
     </div>
   );
 }
